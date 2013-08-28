33561b14777d0fe4abe5142fbe2a970ad7b61c85
meenababu <meena.mothish@gmail.com>
2013-08-28 17:06:33 +0100

    added noteclass views

--- js/NoteClass.js
+++ js/NoteClass.js
@@ -9,53 +9,6 @@ $(document).ready(function(){
 		var noteJsonArr = null;		
 		var noteClassSearch = null;
 
-		//Function to print the search results on HTML page
-		function PrintNoteResults (NoteResultsArr){
-
-			var NoteTableBody = document.createElement("tbody");
-
-			for (var i=0; i<NoteResultsArr.length; i++) {
-
-				for (var key in NoteResultsArr[i]) {
-
-					if (NoteResultsArr[i].hasOwnProperty(key)) {
-									 	
-						var NoteRow  = document.createElement("tr");
-
-						//Creating column for holding the keys
-						var NoteCellKey = document.createElement("td");
-						var NoteCellKeyText = document.createTextNode(key + " : ");
-						NoteCellKey.appendChild(NoteCellKeyText);
-						NoteRow.appendChild(NoteCellKey);
-
-						//Creating column for holding the values
-						var NoteCellVal = document.createElement("td");
-						var NoteCellValText = "";
-						if (NoteResultsArr[i][key] instanceof Object == true){
-							var NoteValObj = NoteResultsArr[i][key]._data;
-							console.log(NoteResultsArr[i][key]);
-							console.log(NoteValObj);
-							NoteCellValText = document.createTextNode (NoteValObj);  
-						}
-						else {
-							NoteCellValText = document.createTextNode (NoteResultsArr[i][key]);
-					    }
-										   
-						NoteCellVal.appendChild(NoteCellValText);
-						NoteRow.appendChild(NoteCellVal);
-
-						//Appending the row to table body and the table body to the table
-						NoteTableBody.appendChild(NoteRow);
-						NoteTable.appendChild(NoteTableBody);
-									
-					}
-								
-				}
-
-			}
-		
-		}
-		
 		//Reading json file using getJson method & making the GET calls to run searches
 		$.getJSON('json/metaspec.json',function(notejson){
 				        
@@ -97,27 +50,15 @@ $(document).ready(function(){
 						var SearchVal = document.getElementById("noteClass_SearchBox").value;
 						var SearchValId = null;
 
-						//document.getElementById("NoteTable").innerHTML = "";
-						//document.getElementById("noteResults").style.visibility = "visible";
-						//document.getElementById("NoteTable").style.visibility = "visible";
+						document.getElementById("note_results").innerHTML = "";
+						document.getElementById("noteResults").style.visibility = "visible";
 						
-							
+						
+
+						//Can.View for rendering search results onto the HTML page	
+
+
 						//Searching for 'All'
-						/*
-						if ((SearchVal != null) && (SearchVal == "All")){
-							var NoteResultsArr = [];
-							noteClassSearch.findAll( {}, function(data){
-
-							for (var i =0; i<data.length; i++){
-									console.log(data[i]._data);
-									NoteResultsArr.push(data[i]._data);  //storing the results for 'All' search in an array
-								}
-							PrintNoteResults(NoteResultsArr);
-
-							});
-
-						}*/
-		/////////Can.View for rendering search results onto the HTML page	
 						if ((SearchVal !== null) && (SearchVal == "All")){		
 							var NoteResultsArr;	
 							noteClassSearch.findAll( {}, function(data){
@@ -126,16 +67,13 @@ $(document).ready(function(){
 								for (var i =0; i<data.length; i++){
 										NoteResultsArr.push(data[i]._data);  //storing the results for 'All' search in an array
 									}
-									var NoteResultsStr = JSON.stringify(NoteResultsArr);
-									console.log(NoteResultsStr);
-								var NoteClassView = can.view('NoteClass.ejs', NoteResultsStr);
-								console.log(NoteClassView);
-								console.log(document.getElementById("NoteTable").innerHTML);
-								document.getElementById("NoteTable").appendChild(NoteClassView);
-								console.log(document.getElementById("NoteTable").innerHTML);
+									
+								var NoteClassView = can.view('views/NoteClassView.ejs', NoteResultsArr);
+								document.getElementById("note_results").appendChild(NoteClassView);
+
 							})
 						}
-///////////////////////////////
+						
 						//Searching for a specific Note Class
 
 						else if(SearchVal != null) {
@@ -148,12 +86,10 @@ $(document).ready(function(){
 								}
 							}
 							noteClassSearch.findOne( {id: SearchValId}, function(data){
-								console.log("Results for " + SearchVal);
-								console.log(data._data);
 								NoteResultsArr.push(data._data); //storing the results for search of specific noteId
-								console.log(NoteResultsArr);
-								PrintNoteResults(NoteResultsArr);
-								}); 		
+								var NoteClassView = can.view('views/NoteClassView.ejs', NoteResultsArr);
+								document.getElementById("note_results").appendChild(NoteClassView);								
+							}); 		
 
 						}
 						
@@ -161,6 +97,7 @@ $(document).ready(function(){
 	
 				})();
 			});
+
 
 			//Adding the source for typeahead to the search text box
 			$("#noteClass_SearchBox").typeahead({
@@ -170,9 +107,7 @@ $(document).ready(function(){
 			
 			//Clearing the table of Note class results, if the text box value is changed.
 		$("#noteClass_SearchBox").on('change',function(e){
-			    //document.getElementById("note_results").innerHTML = "";
-			    //document.getElementById("noteResults").style.visibility = "hidden";
-				//document.getElementById("NoteTable").style.visibility = "hidden";
+			    document.getElementById("note_results").innerHTML = "";
 
 		});
 			
